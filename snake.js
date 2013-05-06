d1411bec2cd58f4b904873743ffcf0a6a028f957
whncode <wh1t3en@gmail.com>
2013-05-06 22:20:27 +0200

    add prototypes

--- snake.js
+++ snake.js
@@ -1,160 +1,130 @@
-
-
-var snake = function (name, x, y){
+var HipsterSnake = {
+  Snake : function (name, x, y){
     this.poses = [];
     this.poses.push(new pos(x,y,null)); 
     this.len = 100;
-    this.name=name;
+    this.name = name;
     this.angle = 4;
     this.color = '#ff0000';
-
-    this.goTo = function(x, y) {
-        var new_pos = new pos(this.getHead().x, this.getHead().y,this.getHead());
-        this.poses.push(new_pos); 
-    }
-
-    this.look = function (x, y) {
-        var dx = x - this.getHead().x;
-        var dy = this.getHead().y - y;
-        this.angle = Math.atan2(dx,dy);
-    }
-
-    this.move = function (steps) {
-        var new_x = Math.sin(this.angle) * steps;
-        var new_y = Math.cos(this.angle) * steps;
-        
-        this.getHead().x += new_x;
-        this.getHead().y -= new_y;
-    }
-
-    this.getHead = function () {
-        return this.poses[this.poses.length - 1];
-    }
-  
-    var refreshTail = function () {
-        var positions = this.positions.slice();
-        var len = 0;
-    }
-
-    this.isOut = function (){
-        
-    }
-
-    this.drawMe = function (ctx) {
-
-        //ctx.stroke();
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = "#000000";
-        ctx.arc(this.getHead().x, this.getHead().y, 10, this.angle + Math.PI, Math.PI*2 + this.angle, false); 
-
-        //ctx.strokeStyle = "#00FF00"; 
-        ctx.stroke(); 
-        ctx.lineCap = "round";
-
-        ctx.moveTo(this.getHead().x, this.getHead().y);
-
-        ctx.strokeStyle = this.color; 
-
-        
-        ctx.lineWidth = 4;
-
-        for(var i = this.poses.length - 1; i >= 0; i--){
-            ctx.lineTo(this.poses[i].x, this.poses[i].y); 
-        }
-
-        ctx.stroke();
-        
-    }
-
-    function pos(x, y, last) {
-        this.x = x;
-        this.y = y;
-        this.last = last;
+  },
+  Board : function(ctx){
+    this.snakes = [];
+    this.ctx = ctx;
     
-        this.lenTo = function(pos) {
-            var dx = Math.abs(this.x - pos.x);
-            var dy = Math.abs(this.y - pos.y);
-            return Math.sqrt(Math.pow(dx,2)+Math.pow(xy,2)); 
-        }
-    }
-}
-//testy
+    var snake = HipsterSnake.Snake;
+  }
 
+};
 
-var space = function(ctx) {
-    var snakes = [];
-    var ctx = ctx;
+HipsterSnake.Snake.prototype.goTo = function(x, y) {
+  var new_pos = new pos(this.getHead().x, this.getHead().y,this.getHead());
+  this.poses.push(new_pos); 
+};
 
-    function drawBackground(){
-        ctx.fillStyle = "#fef";
-        ctx.fillRect(0,0,500, 500);
-    }
+HipsterSnake.Snake.prototype.look = function (x, y) {
+  var dx = x - this.getHead().x;
+  var dy = this.getHead().y - y;
+  this.angle = Math.atan2(dx,dy);
+};
 
-    function drawSnake(snake){
-        newLine();
-        ctx.lineCap = "round";
-        //draw head
-        ctx.lineWidth = 3;
-        ctx.strokeStyle = "#000000";
-        ctx.arc(snake.getHead().x, snake.getHead().y, 10, snake.angle + Math.PI, Math.PI*2 + snake.angle, false); 
-    
-        //draw tail
-        ctx.moveTo(snake.getHead().x, snake.getHead().y);
-        ctx.strokeStyle = snake.color; 
-        ctx.lineWidth = 4;
-        for(var i = snake.poses.length - 1; i >= 0; i--){
-            ctx.lineTo(snake.poses[i].x, snake.poses[i].y); 
-        }
-        ctx.stroke();
-        newLine();
+HipsterSnake.Snake.prototype.move = function (steps) {
+  var new_x = Math.sin(this.angle) * steps;
+  var new_y = Math.cos(this.angle) * steps;
 
-    } 
-    
-        /**
-         *  hack to draw new path
-         **/
-    function newLine(){
-        ctx.beginPath();
-        ctx.closePath();
-        }
-    
-}
+  this.getHead().x += new_x;
+  this.getHead().y -= new_y;
+
+  return {'x' : new_x, 'y' : new_y};
+};
+
+HipsterSnake.Snake.prototype.getHead = function () {
+  return this.poses[this.poses.length - 1];
+};
+
+HipsterSnake.Snake.prototype.refreshTail = function () {
+  var positions = this.positions.slice();
+  var len = 0;
+};
+
+var pos = function (x, y, last) {
+  this.x = x;
+  this.y = y;
+  this.last = last;
+
+  this.lenTo = function(pos) {
+    var dx = Math.abs(this.x - pos.x);
+    var dy = Math.abs(this.y - pos.y);
+    return Math.sqrt(Math.pow(dx,2)+Math.pow(xy,2)); 
+  }
+};
+
+HipsterSnake.Board.prototype.newSnake = function(name){
+      this.snakes.push(new snake(name,100,100));
+};
+
+HipsterSnake.Board.prototype.drawAll = function(){
+      drawBackground();
+      snakes.forEach(drawSnake);
+};
+
+HipsterSnake.Board.prototype.drawBackground = function (){
+      this.ctx.fillStyle = "#fef";
+      this.ctx.fillRect(0,0,500, 500);
+};
+
+HipsterSnake.Board.prototype.drawSnake = function (snake){
+      newLine();
+      this.ctx.lineCap = "round";
+      this.ctx.lineWidth = 3;
+      this.ctx.strokeStyle = "#000000";
+      this.ctx.arc(snake.getHead().x, snake.getHead().y, 10, snake.angle + Math.PI, Math.PI*2 + snake.angle, false); 
+
+      //draw tail
+      newLine();
+      this.ctx.moveTo(snake.getHead().x, snake.getHead().y);
+      this.ctx.strokeStyle = snake.color; 
+      this.ctx.lineWidth = 4;
+      for(var i = snake.poses.length - 1; i >= 0; i--){
+        ctx.lineTo(snake.poses[i].x, snake.poses[i].y); 
+      }
+      ctx.stroke();
+}; 
 
 
 var canvas = document.getElementById('drawSpace');
 var ctx = canvas.getContext('2d');
 
-var new_snake = new snake('snake1',200,200);
-new_snake.goTo(30,30);
-new_snake.drawMe(ctx);
+board1 = new HipsterSnake.Board(ctx);
+board1.newSnake('STEFAN');
+board1.drawAll();
 
 canvas.onclick = function (event){
-    var x, y;
+  var x, y;
 
-    x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(canvas.offsetLeft);
-    y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(canvas.offsetTop) + 1;
+  x = event.clientX + document.body.scrollLeft + document.documentElement.scrollLeft - Math.floor(canvas.offsetLeft);
+  y = event.clientY + document.body.scrollTop + document.documentElement.scrollTop - Math.floor(canvas.offsetTop) + 1;
 
-    new_snake.look(x,y);
-    new_snake.goTo(x,y);
+  new_snake.look(x,y);
+  new_snake.goTo(x,y);
 }
 
 //everyhing bottom to refactor
 window.requestAnimFrame = (function(callback) {
-        return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
-        function(callback) {
-          window.setTimeout(callback, 1000 / 60);
-        };
-      })();
+  return window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.oRequestAnimationFrame || window.msRequestAnimationFrame ||
+  function(callback) {
+    window.setTimeout(callback, 1000 / 60);
+  };
+})();
 
 
 
 
 function animate() {
-        new_snake.move(1);
-        new_snake.drawMe(ctx);
-        // request new frame
-        requestAnimFrame(function() {
-          animate();
-        });
-      }
+  new_snake.move(1);
+  new_snake.drawMe(ctx);
+  // request new frame
+  requestAnimFrame(function() {
+    animate();
+  });
+}
 animate();
